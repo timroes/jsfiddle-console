0f7af9e2647de148d023c0faac562364260bd543
Tim Roes <tim.roes@inovex.de>
2016-03-12 21:42:00 +0100

    remove jquery dependency

--- console.js
+++ console.js
@@ -1,6 +1,6 @@
-var out = $('#output');
+var out = document.getElementById('output');
 
 console.log = function(str) {
-	var line = '<div><span style="color: #999">&gt;</span> ' + str + '</div>';
-	out.append(line);
+	var line = '<div style="font-family:monospace;line-height:1.7"><span style="color: #999">&gt;</span> ' + str + '</div>';
+	out.innerHTML += line;
 };
